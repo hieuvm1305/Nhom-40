5645a245567dc6d205e01577cf22ed0763e30ed5
hieuvm1305 <61863560+hieuvm1305@users.noreply.github.com>
2020-05-12 12:45:24 +0700

    Stage 2

---
+++ Stage_2.java
@@ -0,0 +1,35 @@
+package btaplon;
+import java.awt.Graphics;
+public class Stage_2 extends GameState {
+    public Stage_2(GameStateManager gsm){
+        super(gsm);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void keyPressed(int k) {
+
+    }
+
+    @Override
+    public void keyReleased(int k) {
+
+    }
+
+
+
+}
